7d63a0d2dfe786babe78e70348b74920179fbf14
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-05-09 20:15:36 +0300

    Add data queries

--- src/routes/schedule.php
+++ src/routes/schedule.php
@@ -2,7 +2,37 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+function getSection($db, $id) {
+    $query = "SELECT
+                   sport_kind_id as id,
+                   kind_title as title
+                FROM sport_kinds 
+                WHERE sport_kind_id = :section_id;";
+    $stmt = $db->prepare($query);
+    $stmt->execute(['section_id' => $id]);
+    return $stmt->fetch();
+}
+
 $app->get('/api/schedule/{section_id}', function (Request $request, Response $response, $args) {
+    $data['section'] = getSection($this->db, $args['section_id']);
+
+    $query = "SELECT DISTINCT
+                    t.teacher_id as id,
+                    t.teacher_name as name,
+                    t.teacher_surname as surname,
+                    t.teacher_thname as middleName
+                FROM
+                    teachers as t 
+                JOIN trainings as tr 
+                    ON t.teacher_id = tr.teacher_id
+                JOIN sport_kinds as s
+                    ON tr.sport_kind_id = s.sport_kind_id
+                WHERE s.sport_kind_id = :section_id
+                ORDER BY teacher_surname";
+    $stmt = $this->db->prepare($query);
+    $stmt->execute($args);
+    $data['teachers'] = $stmt->fetchAll();
+
     $query = "SELECT 
                    teach.teacher_name as name, 
                    teach.teacher_surname as surname, 
@@ -18,18 +48,28 @@ $app->get('/api/schedule/{section_id}', function (Request $request, Response $re
                     ON t.location_id = l.location_id
                 JOIN teachers as teach
                     ON teach.teacher_id = t.teacher_id
-                WHERE t.sport_kind_id = :section_id";
+                WHERE t.sport_kind_id = :section_id;";
     $stmt = $this->db->prepare($query);
     $stmt->execute($args);
-    $data = [
-        'sectionId' => $args['section_id'],
-        'sectionTitle' => 'TITLE',
-        'schedule' => $stmt->fetchAll(),
-    ];
+    $data['schedule'] = $stmt->fetchAll();
+
     return $response->withJson($data);
 });
 
 $app->get('/api/schedule/{section_id}/{teacher_id}', function (Request $request, Response $response, $args) {
+    $data['section'] = getSection($this->db, $args['section_id']);
+
+    $query = "SELECT
+                   teacher_id as id,
+                   teacher_name as name,
+                   teacher_surname as surname,
+                   teacher_thname as middleName
+                FROM teachers
+                WHERE teacher_id = :teacher_id;";
+    $stmt = $this->db->prepare($query);
+    $stmt->execute(['teacher_id' => $args['teacher_id']]);
+    $data['teacher'] = $stmt->fetch();
+
     $query = "SELECT
                     t.lesson_num as lesson,
                     t.day_num as day,
@@ -41,16 +81,7 @@ $app->get('/api/schedule/{section_id}/{teacher_id}', function (Request $request,
                 WHERE t.teacher_id = :teacher_id AND t.sport_kind_id = :section_id";
     $stmt = $this->db->prepare($query);
     $stmt->execute($args);
-    $data = [
-        'sectionId' => $args['section_id'],
-        'sectionTitle' => 'TITLE',
-        'teacher' => [
-            'id' => $args['teacher_id'],
-            'name' => '',
-            'surname' => '',
-            'middleName' => '',
-        ],
-        'schedule' => $stmt->fetchAll(),
-    ];
+    $data['schedule'] = $stmt->fetchAll();
+
     return $response->withJson($data);
 });
--- src/routes/sections.php
+++ src/routes/sections.php
@@ -3,7 +3,11 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/api/sections', function (Request $request, Response $response) {
-    $query = 'SELECT * FROM sport_kinds ORDER BY kind_title';
+    $query = 'SELECT
+                    sport_kind_id as sectionId,
+                    kind_title as title
+                FROM sport_kinds
+                ORDER BY kind_title';
     $data = $this->db->query($query)->fetchAll();
     return $response->withJson($data);
 });
