0fad8b3db30720b3e5556553c302c838e7fa38c8
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-05-09 17:08:50 +0300

    Update DB config

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ function config_db () {
                                 "charset=utf8";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
         $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'), $options);
