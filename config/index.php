4d617b750dbbae8330604a9e21127aa9e5fa5af1
Yakiv Vakoliuk <yahacomua@gmail.com>
2020-05-09 14:54:56 +0300

    Fix dotenv config

--- config/index.php
+++ config/index.php
@@ -1,8 +1,6 @@
 <?php
 require_once('../vendor/autoload.php');
 
-$dotEnv = Dotenv\Dotenv::createImmutable('../');
-
-$dotEnv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS'])->notEmpty();
-
+$dotEnv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotEnv->load();
+$dotEnv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS'])->notEmpty();
